4145b872cf8edc593a4bce057993632a1e1359c0
Matheus Henrique <matheus_hoq.contato@outlook.com>
2021-09-28 14:58:39 -0300

    ormconfig + database

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,9 @@
+import "reflect-metadata";
 import express from "express";
 
-// @types/express
+import "./database";
+
 const app = express();
-
-/**
- * GET => Buscar uma informação
- * POST => Inserir (criar) uma informação
- * PUT => Alterar uma informação
- * DELETE => Remover um dado
- * PATCH => Alterar uma informação específica
- *  **/
-
-/** 
- * Tipos de parâmetros
- * Routes Params => http://localhost:3000/produtos/16543161531
- * Query Params => http://localhost:3000/produtos?name=teclado&tecladobom
- * 
- * Body Params => {
- *  "name": "teclado",
- *  "description": "teclado bom"
- * }
- * 
- *  */
-
-app.get("/test", (request, response) => {
-  //Request => Entrando
-  //Response => Saindo
-  return response.send("Olá, NLW!");
-});
-
-app.post("/test-post", (request, response) => {
-    return response.send("Olá, NLW - Método POST")
-})
 
 // http://localhost:3000
 app.listen(3000, () => console.log("Server rodando"));
--- testes/teste.ts
+++ testes/teste.ts
@@ -10,3 +10,26 @@
 //     email: "matheus.querido@outlook.com",
 //     nome: "Matheus",
 // });
+
+/**
+ * GET => Buscar uma informação
+ * POST => Inserir (criar) uma informação
+ * PUT => Alterar uma informação
+ * DELETE => Remover um dado
+ * PATCH => Alterar uma informação específica
+ *  **/
+
+/** 
+ * Tipos de parâmetros
+ * Routes Params => http://localhost:3000/produtos/16543161531
+ * Query Params => http://localhost:3000/produtos?name=teclado&tecladobom
+ * 
+ * Body Params => {
+ *  "name": "teclado",
+ *  "description": "teclado bom"
+ * }
+ * 
+ *  */
+
+  //Request => Entrando
+  //Response => Saindo
